e2605bf089e93eb39a32ee71432d27a5a6c436e7
Herbert Roider <herbert@roider.at>
2025-03-07 21:40:10 +0100

    trial

--- src/org/roiderh/dnc/toolbar/DncAction.java
+++ src/org/roiderh/dnc/toolbar/DncAction.java
@@ -17,7 +17,7 @@
 package org.roiderh.dnc.toolbar;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+//import java.awt.event.ActionListener;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
